84d2723e9e0e4aecdccf3e51ae0dc0c27aec6cfc
a <2750503567@qq.com>
2023-04-18 16:51:37 +0800

    on dev

--- packages/caliper-tests-integration/dp-chain_tests/config/dper_dper3/perpare.sh
+++ packages/caliper-tests-integration/dp-chain_tests/config/dper_dper3/perpare.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cp /dpchain/dper/client/project/dperClient  /data/dperClient && ./data/dperClient -auto 
+cp /dpchain/dper/client/auto/dper_dper1/dperClient  /data/dperClient
+cp /dpchain/dper/client/auto/dper_dper1/daemon  /data/daemon 
+cp /dpchain/dper/client/auto/dper_dper1/daemonClose  /data/daemonClose
+cd /data
+sleep 5s
+./dperClient -mode=multi_http
